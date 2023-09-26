c89f3db29259a1838e2045e997d25a410b8bf706
st1llsane <notescape262@gmail.com>
2023-09-26 02:17:00 +0200

    feat: prettier config and imports prettier

--- src/global/Layout.tsx
+++ src/global/Layout.tsx
@@ -1,32 +1,31 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './global.css'
 import { cn } from 'shared/classNames'
 import { Header } from 'widgets/header'
+import './global.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-	title: 'KeyToCap Store',
-	description: 'A store selling keyboards',
+  title: 'KeyToCap Store',
+  description: 'A store selling custom keyboards',
 }
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode
+  children: React.ReactNode
 }) {
-	return (
-		<html lang='en'>
-			<body
-				className={cn(
-					inter.className,
-					'w-full max-w-[1230px] mx-auto px-[15px]'
-				)}
-			>
-				<Header />
-				{children}
-			</body>
-		</html>
-	)
+  return (
+    <html lang='en'>
+      <body
+        className={cn(
+          inter.className,
+          'w-full max-w-[1230px] mx-auto px-[15px]',
+        )}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  )
 }
