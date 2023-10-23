463e9048922eb74e3d565fba860a00871c958bba
st1llsane <notescape262@gmail.com>
2023-10-24 00:31:34 +0200

    chore: working on nav

--- src/shared/constants/buttons.ts
+++ src/shared/constants/buttons.ts
@@ -1,4 +1,3 @@
 export const BUTTON = {
-  NAV_ITEM: ['Keyboards', 'Mouses', 'Mouse pads', 'Keycaps'],
   SHOW_ME: 'Show me!'
 }
--- src/widgets/navigation-menu.tsx
+++ src/widgets/navigation-menu.tsx
@@ -69,7 +69,7 @@ const NavigationMenuContent = React.forwardRef<
   <NavigationMenuPrimitive.Content
     ref={ref}
     className={cn(
-      'left-0 top-0 w-full min-w-[488px] p-4 data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-[488px] data-[motion=from-start]:slide-in-from-left-[488px] data-[motion=to-end]:slide-out-to-right-[488px] data-[motion=to-start]:slide-out-to-left-[488px] md:absolute md:w-auto',
+      'left-0 top-0 w-full min-w-[490px] !duration-200 data-[motion^=from-]:animate-in data-[motion^=to-]:animate-out data-[motion^=from-]:fade-in data-[motion^=to-]:fade-out data-[motion=from-end]:slide-in-from-right-[490px] data-[motion=from-start]:slide-in-from-left-[490px] data-[motion=to-end]:slide-out-to-right-[490px] data-[motion=to-start]:slide-out-to-left-[490px] md:absolute md:w-auto',
       className
     )}
     {...props}
