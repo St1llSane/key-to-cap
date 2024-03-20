e2ec780a52e95ef3e448dd27249cde503b0157eb
st1llsane <notescape262@gmail.com>
2024-03-20 16:43:21 +0200

    refactor: deleted pages directory to fix error with QueryClientProvider

--- global.d.ts
+++ global.d.ts
@@ -1,21 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 declare module '*.png' {
-  const value: any;
-  export default value;
+  const value: any
+  export default value
 }
 
 declare module '*.jpg' {
-  const value: any;
-  export default value;
+  const value: any
+  export default value
 }
 
 declare module '*.jpeg' {
-  const value: any;
-  export default value;
+  const value: any
+  export default value
 }
 
 declare module '*.webp' {
-  const value: any;
-  export default value;
+  const value: any
+  export default value
 }
